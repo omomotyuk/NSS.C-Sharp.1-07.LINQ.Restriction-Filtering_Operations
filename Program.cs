9fe7a789bec1c3577fcb5721aea333d2fde03a58
Alex Momotyuk <o.a.momotyuk@gmail.com>
2019-11-20 22:11:15 -0600

    L-letter fruits practice - done

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1_07.LINQ.Restriction_Filtering_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Find the words in the collection that start with the letter 'L'
+            List<string> fruits = new List<string>() {"Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry"};
+
+            IEnumerable<string> LFruits = from fruit in fruits
+                where fruit.StartsWith("L")
+                orderby fruit ascending
+                select fruit;
+            foreach( string fruit in LFruits )
+            {
+                Console.WriteLine($"{fruit}");
+            }
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# NSS.C-Sharp.1-07.LINQ.Restriction-Filtering_Operations
